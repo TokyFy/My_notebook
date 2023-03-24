7ce42ed11a5aa4957035b7d51d12b5a43093bd2c
Toky <tokyfydev@gmail.com>
2023-03-24 15:39:37 +0100

    :sparkles: - Add Dark mode - Change avatars to a cat - Add locomotive scrooll

---
+++ src/context/themeContext.ts
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+const ThemeContext = createContext("LIGHT");
+export default ThemeContext;
--- src/image/index.ts
+++ src/image/index.ts
@@ -7,5 +7,8 @@ import notebookImage from "./notebook.png";
 import penImage from "./pen.png";
 import toolsImage from "./tools.png";
 import trainingImage from "./trainning.png";
+import heartImage from "./blackHeart.png";
+import avatarLight from "./avatar-ligth.png";
+import avatar from "./avatar.png";
 
-export {notebookImage,trainingImage,toolsImage,penImage,musicImage,keyboardImage,moviesImage,craftImage,gameImage}
+export {notebookImage,trainingImage,toolsImage,penImage,musicImage,keyboardImage,moviesImage,craftImage,gameImage , heartImage , avatarLight , avatar}
